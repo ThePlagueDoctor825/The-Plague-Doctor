d0f91c06d4c712601bbe5f1d17d3a123677de050
ThePlagueDoctor825 <bohdankiforharchenko@gmail.com>
2022-09-11 17:05:37 +0300

    dz 1

--- class2.py
+++ class2.py
@@ -6,7 +6,9 @@ class Student:
         self.name = name
         self.gladness = 50
         self.progress = 0
+        self.money = 10
         self.alive = True
+
 
     def to_study(self):
         print("Time to study")
@@ -32,22 +34,40 @@ class Student:
         elif self.progress > 5:
             print("Done!")
             self.alive = False
+    def work(self):
+        if self.gladness >= 110:
+            self.money += 30
+            print("Student get money")
+    def student_money(self):
+        if self.money <= 0:
+            print("you got into debt")
+            self.alive = False
+        elif self.money >= 0:
+            print("Nice")
+    def spend_money(self):
+        if self.money >= 200:
+          self.money -= random.randint(20,200)
+
     def end_of_day(self):
         print(f"Gladness = {self.gladness}")
         print(f"Progress = {round(self.progress, 2)}")
+        print(f"Money = {self.money}")
 
     def life(self, day):
         day = "Day" +str(day)+"of"+self.name+"life"
         print(f"{day:=^50}")
-        live_cube = random.randint(1,3)
+        live_cube = random.randint(1, 4)
         if live_cube == 1:
             self.to_study()
         elif live_cube == 2:
             self.to_sleep()
         elif live_cube == 3:
             self.to_chill()
+        elif live_cube == 4:
+            self.work()
         self.end_of_day()
         self.is_alive()
+
 
 JoeBiden = Student(name="Joe Biden")
 for day in range(365):
